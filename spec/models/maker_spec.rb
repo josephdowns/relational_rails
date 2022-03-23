b8636cc2c920ac583a518e97022635c9bca44b8c
Joseph Downs <jos.w.downs@gmail.com>
2022-03-22 22:10:48 -0600

    Add false test

--- spec/models/maker_spec.rb
+++ spec/models/maker_spec.rb
@@ -8,6 +8,10 @@ RSpec.describe Maker, type: :model do
         expect(maker.country).to eq("Go USA!")
       end
 
+      it "returns a different message when false" do
+        maker = Maker.create!(name: "BMW", domestic: false, year_founded: 1916)
+        expect(maker.country).to eq("Well...that's cool too.")
+      end
     end
   end
 end
