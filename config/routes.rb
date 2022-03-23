0a5953627a29d920e4e552e14a0b347ee1eae8a1
Joseph Downs <jos.w.downs@gmail.com>
2022-03-23 17:29:59 -0600

    Add route for cars/show

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/makers', to: 'makers#index'
   get '/makers/:id', to: 'makers#show'
   get '/cars', to: 'cars#index'
+  get '/cars/:id', to: 'cars#show'
 end
