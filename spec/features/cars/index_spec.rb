99210285f9170c629644e210d5001aa870cfd221
Joseph Downs <jos.w.downs@gmail.com>
2022-03-23 15:10:25 -0600

    Add index test

--- spec/features/cars/index_spec.rb
+++ spec/features/cars/index_spec.rb
@@ -7,5 +7,16 @@
 require 'rails_helper'
 
 RSpec.describe "car index page", type: :feature do
-  
+  before (:each) do
+    @maker = Maker.create!(name: "Dodge", domestic: true, year_founded: 1900)
+    @car_1 = Car.create!(name: "", color: "", doors: 4, in_production: true, maker_id: @maker.id)
+    @car_2 = Car.create!(name: "", color: "", doors: 4, in_production: true, maker_id: @maker.id)
+  end
+
+  it "displays each car name and makers name" do
+    vist '/cars'
+    expect(page).to have_content(@maker.name)
+    expect(page).to have_content(@car_1.name)
+    expect(page).to have_content(@car_2.name)
+  end
 end
