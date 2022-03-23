2a9384daea6172db312a07b17042dda5aea6e407
Joseph Downs <jos.w.downs@gmail.com>
2022-03-22 23:30:54 -0600

    Add maker.year_founded test

--- spec/features/makers/show_spec.rb
+++ spec/features/makers/show_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'the makers show page' do
-  it "displays the makers name" do
-    maker_1 = Maker.create!(name: "Dodge", domestic: true, year_founded: 1900)
-
-    visit "/makers/#{maker_1.id}"
-    expect(page).to have_content(maker_1.name)
+  before (:each) do
+    @maker_1 = Maker.create!(name: "Dodge", domestic: true, year_founded: 1900)
   end
 
-  it "displays the makers year_founded"
+  it "displays the makers name" do
+    visit "/makers/#{@maker_1.id}"
+    expect(page).to have_content(@maker_1.name)
+  end
+
+  it "displays the makers year_founded" do
+    visit "/makers/#{@maker_1.id}"
+    expect(page).to have_content(@maker_1.year_founded)
+  end
 end
