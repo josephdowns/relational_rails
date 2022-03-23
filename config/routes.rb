058c0e028a6dff1eed56d51a6fbe939d98fea674
Joseph Downs <jos.w.downs@gmail.com>
2022-03-23 15:36:10 -0600

    Complete index test

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/makers', to: 'makers#index'
   get '/makers/:id', to: 'makers#show'
+  get '/cars', to: 'cars#index'
 end
--- spec/features/cars/index_spec.rb
+++ spec/features/cars/index_spec.rb
@@ -9,13 +9,14 @@ require 'rails_helper'
 RSpec.describe "car index page", type: :feature do
   before (:each) do
     @maker = Maker.create!(name: "Dodge", domestic: true, year_founded: 1900)
-    @car_1 = Car.create!(name: "", color: "", doors: 4, in_production: true, maker_id: @maker.id)
-    @car_2 = Car.create!(name: "", color: "", doors: 4, in_production: true, maker_id: @maker.id)
+    @car_1 = Car.create!(name: "Durango", color: "black", doors: 4, in_production: true, maker_id: @maker.id)
+    @car_2 = Car.create!(name: "Aries", color: "beige", doors: 2, in_production: false, maker_id: @maker.id)
   end
 
   it "displays each car name and makers name" do
-    vist '/cars'
-    expect(page).to have_content(@maker.name)
+    visit '/cars'
+    save_and_open_page
+    # expect(page).to have_content(@maker.name)
     expect(page).to have_content(@car_1.name)
     expect(page).to have_content(@car_2.name)
   end
