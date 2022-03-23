a01634fcec9ea33c681d896049cffc95eca3b159
Joseph Downs <jos.w.downs@gmail.com>
2022-03-23 14:38:53 -0600

    Switch to main branch

---
+++ spec/features/cars/index_spec.rb
@@ -0,0 +1,11 @@
+# User Story 3, Child Index
+#
+# As a visitor
+# When I visit '/child_table_name'
+# Then I see each Child in the system including the Child's attributes:
+
+require 'rails_helper'
+
+RSpec.describe "car index page", type: :feature do
+  
+end
