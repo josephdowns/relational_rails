d5b98f79c96423d48dbc4d29d21cca800e653392
Joseph Downs <jos.w.downs@gmail.com>
2022-03-24 22:04:50 -0600

    Add created by test

--- spec/features/makers/makers_index_spec.rb
+++ spec/features/makers/makers_index_spec.rb
@@ -8,6 +8,19 @@ RSpec.describe "maker index page", type: :feature do
     visit "/makers"
     expect(page).to have_content(maker_1.name)
     expect(page).to have_content(maker_2.name)
+  end
 
+  #   As a visitor
+  # When I visit the parent index,
+  # I see that records are ordered by most recently created first
+  # And next to each of the records I see when it was created
+  it "displays timestamp of created by" do
+    maker_1 = Maker.create!(name: "Dodge", domestic: true, year_founded: 1900)
+    maker_2 = Maker.create!(name: "BMW", domestic: false, year_founded: 1916)
+
+    visit "/makers"
+    save_and_open_page
+    expect(page).to have_content(maker_1.created_at)
+    expect(page).to have_content(maker_2.created_at)
   end
 end
