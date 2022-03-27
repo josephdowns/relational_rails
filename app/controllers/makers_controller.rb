2fa88ed857624f9497cd6cb0e412029765b07035
Joseph Downs <jos.w.downs@gmail.com>
2022-03-27 00:58:49 -0600

    Add create method

--- app/controllers/makers_controller.rb
+++ app/controllers/makers_controller.rb
@@ -7,4 +7,19 @@ class MakersController < ApplicationController
     @maker = Maker.find(params[:id])
     @count = @maker.cars_count
   end
+
+  def new
+  end
+
+  def create
+    maker = Maker.new({
+      name: params[:maker][:name],
+      domestic: params[:maker][:domestic],
+      year_founded: params[:maker][:year_founded]
+      })
+
+    maker.save
+
+    redirect_to '/makers'
+  end
 end
