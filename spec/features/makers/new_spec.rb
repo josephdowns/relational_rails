a0c2c05469e8267e7f678ce89f68266c8ea8a1cf
Joseph Downs <jos.w.downs@gmail.com>
2022-03-27 16:30:57 -0600

    Add new spec for maker

---
+++ spec/features/makers/new_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "maker creation page", type: :feature do
+
+
+  it "links me to a makers/new" do
+    visit "/makers"
+    click_on "New Maker"
+
+    expect(current_path).to eq("/makers/new")
+  end
+
+  it "should create a new maker" do
+    visit '/makers/new'
+# save_and_open_page
+    fill_in('maker[name]', with: "Testla")
+    fill_in('maker[year_founded]', with: 2003)
+    fill_in('maker[domestic]', with: true)
+    click_button "Create Maker"
+
+    new_maker_id = Maker.last.id
+    expect(current_path).to eq("/makers")
+    expect(page).to have_content("Testla")
+
+  end
+end
