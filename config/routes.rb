2469064e8b729debb7096e04cab5970519d3ca3b
Joseph Downs <jos.w.downs@gmail.com>
2022-03-27 01:08:58 -0600

    Finish user story 11

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/makers', to: 'makers#index'
+  get '/makers/new', to: 'makers#new'
+  post '/makers', to: 'makers#create'
   get '/makers/:id', to: 'makers#show'
   get '/cars', to: 'cars#index'
   get '/cars/:id', to: 'cars#show'
   get '/makers/:maker_id/cars', to: 'maker_cars#index'
-  get '/makers/new', to: 'makers#new'
 end
--- spec/features/makers/index_spec.rb
+++ spec/features/makers/index_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "maker index page", type: :feature do
+  before (:each) do
+    @maker_1 = Maker.create!(name: "Dodge", domestic: true, year_founded: 1900)
+    @maker_2 = Maker.create!(name: "BMW", domestic: false, year_founded: 1916)
+    @maker_3 = Maker.create!(name: "Ford", domestic: true, year_founded: 1896)
+  end
   it "can list makers' name" do
-    maker_1 = Maker.create!(name: "Dodge", domestic: true, year_founded: 1900)
-    maker_2 = Maker.create!(name: "BMW", domestic: false, year_founded: 1916)
-
     visit "/makers"
-    expect(page).to have_content(maker_1.name)
-    expect(page).to have_content(maker_2.name)
+    expect(page).to have_content(@maker_1.name)
+    expect(page).to have_content(@maker_2.name)
   end
 
   #   As a visitor
@@ -15,31 +17,26 @@ RSpec.describe "maker index page", type: :feature do
   # I see that records are ordered by most recently created first
   # And next to each of the records I see when it was created
   it "displays timestamp of created by" do
-    maker_1 = Maker.create!(name: "Dodge", domestic: true, year_founded: 1900)
-    maker_2 = Maker.create!(name: "BMW", domestic: false, year_founded: 1916)
 
     visit "/makers"
 
-    expect(page).to have_content(maker_1.created_at)
-    expect(page).to have_content(maker_2.created_at)
+    expect(page).to have_content(@maker_1.created_at)
+    expect(page).to have_content(@maker_2.created_at)
   end
 
   it "sorts using order_by_created_at" do
-    maker_1 = Maker.create!(name: "Dodge", domestic: true, year_founded: 1900)
-    maker_2 = Maker.create!(name: "BMW", domestic: false, year_founded: 1916)
-    maker_3 = Maker.create!(name: "Ford", domestic: true, year_founded: 1896)
 
     visit "/makers"
 
-    expect(maker_3.name).to appear_before(maker_2.name)
-    expect(maker_2.name).to appear_before(maker_1.name)
+    expect(@maker_3.name).to appear_before(@maker_2.name)
+    expect(@maker_2.name).to appear_before(@maker_1.name)
   end
 
   it "links me to a makers/new" do
     visit "/makers"
-
     click_on "New Maker"
 
+    save_and_open_page
     expect(current_path).to eq("/makers/new")
   end
 end
