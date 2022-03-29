b9e126a7143f5de681436093b794fce093c4ccef
Joseph Downs <jos.w.downs@gmail.com>
2022-03-29 15:10:02 -0600

    Make test for sort by doors

--- spec/features/makers/cars/index_spec.rb
+++ spec/features/makers/cars/index_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe "associations", type: :feature do
 
     @x5 = @bmw.cars.create!(name: "X5", color: "blue", doors: 4, in_production: true)
     @ix = @bmw.cars.create!(name: "iX", color: "black", doors: 4, in_production: true)
+    @z1 = @bmw.cars.create!(name: "Z1", color: "red", doors: 2, in_production: false)
 
     @neon = @dodge.cars.create!(name: "Neon", color: "white", doors: 4, in_production: true)
     @aries = @dodge.cars.create!(name: "Aries", color: "beige", doors: 2, in_production: false)
@@ -43,5 +44,29 @@ RSpec.describe "associations", type: :feature do
 
       expect(@aries.name).to appear_before(@neon.name)
     end
+    #   As a visitor
+    # When I visit the Parent's children Index Page
+    # I see a form that allows me to input a number value
+    # When I input a number value and click the submit button that reads 'Only return records with more than `number` of `column_name`'
+    # Then I am brought back to the current index page with only the records that meet that threshold shown.
+
+    describe "has a form to input a number" do
+      it "only displays values that exceed that number" do
+        visit "/makers/#{@bmw.id}/cars"
+        expect(page).to have_content("#{@x5.name}")
+        expect(page).to have_content("#{@ix.name}")
+        expect(page).to have_content("#{@z1.name}")
+
+        fill_in "Only show cars with more doors than", with: 3
+        click_on "Submit"
+
+        expect(page).to have_content("#{@x5.name}")
+        expect(page).to have_content("#{@ix.name}")
+        expect(page).to_not have_content("#{@z1.name}")
+
+      end
+    end
   end
+
+
 end
